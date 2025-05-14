8ccf8920ce746a940b3ae6cb11aa80b5ce5d17ba
talnw1123 <talnw1123@gmail.com>
2025-05-14 11:46:48 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/ReelSmart' : '',
 }
 
 export default nextConfig
